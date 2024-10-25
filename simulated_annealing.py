86ce9dacb553127c7c09a66bc537b32371f349ae
Giochelavaipiatti <78697256+bergio13@users.noreply.github.com>
2024-10-25 13:55:59 +0200

    Introduction of delta evaluation

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -2,6 +2,26 @@ import numpy as np
 
 # Simulated Annealing algorithm
 def simulated_annealing(initial_state, objective_function, neighborhood_operator, T_init, cooling_rate=0.95, max_iter=20_000, reheat_threshold=500):
+    """Simulated Annealing algorithm to solve combinatorial optimization problems. 
+    The algorithm iteratively searches for the optimal solution by accepting moves that decrease 
+    the objective function value with a probability that depends on the current temperature and the 
+    magnitude of the change in the objective function value. The algorithm is guaranteed to converge to 
+    the global optimum under certain conditions.
+    
+    
+
+    Args:
+        initial_state (np.array): The initial candidate solution
+        objective_function (function): The objective function to minimize
+        neighborhood_operator (function): A function that generates a neighboring solution
+        T_init (float): The initial temperature
+        cooling_rate (float, optional): Parameter controling the speed of cooling . Defaults to 0.95. 
+        max_iter (int, optional): Maximum number of iterations allowed . Defaults to 20_000.
+        reheat_threshold (int, optional): How many iterations without improvement to wait before reheat . Defaults to 500.
+
+    Returns:
+        np.array: The best solution found by the algorithm
+    """
     t = 0
     current_state = initial_state
     T = T_init
@@ -45,10 +65,81 @@ def simulated_annealing(initial_state, objective_function, neighborhood_operator
         T = T * cooling_rate
         t += 1
         
-        # Restart if stuck in local optima
         # Reheat if stuck in local optima
         if stagnant_iterations >= reheat_threshold:
             T = T_init * 0.75 # Increase the temperature
-            stagnant_iterations = 0  # Reset stagnation counter but keep the best state
+            stagnant_iterations = 0  # Reset stagnation counter
+    
+    return best_state, objective_values, temperatures, accepted_moves
+
+import numpy as np
+
+# Simulated Annealing algorithm
+def simulated_annealing_e(initial_state, objective_function, delta_eval, neighborhood_operator, T_init, cooling_rate=0.95, max_iter=20_000, reheat_threshold=500):
+    """Simulated Annealing algorithm to solve combinatorial optimization problems.
+    The algorithm iteratively searches for the optimal solution by accepting moves that decrease
+    the objective function value with a probability that depends on the current temperature and the
+    magnitude of the change in the objective function value. The algorithm is guaranteed to converge to
+    the global optimum under certain conditions.
+
+    Args:
+        initial_state (np.array): The initial candidate solution
+        objective_function (function): The objective function to minimize
+        delta_eval (function): A function that computes the change in the objective function value
+        neighborhood_operator (function): A function that generates a neighboring solution
+        T_init (float): The initial temperature
+        cooling_rate (float, optional): Parameter controling the speed of cooling . Defaults to 0.95. 
+        max_iter (int, optional): Maximum number of iterations allowed . Defaults to 20_000.
+        reheat_threshold (int, optional): How many iterations without improvement to wait before reheat . Defaults to 500.
+
+    Returns:
+        np.array: The best solution found by the algorithm
+    """
+    
+    t = 0
+    current_state = initial_state
+    T = T_init
+    current_energy = objective_function(current_state)
+    best_state = current_state
+    best_energy = current_energy
+    stagnant_iterations = 0
+    
+    # Lists to store metrics for plotting
+    objective_values = []
+    temperatures = []
+    accepted_moves = []
+    
+    while t < max_iter and current_energy != 0:
+        # get neighbor 
+        swap_pos1, swap_pos2 = neighborhood_operator(current_state)
+        
+        delta_E = delta_eval(current_state, swap_pos1, swap_pos2)
+        
+        if delta_E < 0 or np.random.rand() < np.exp(-abs(delta_E) / T):
+            current_energy += delta_E
+            accepted_moves.append(1)
+            stagnant_iterations = 0
+            
+            if current_energy < best_energy:
+                best_state = np.copy(current_state)
+                best_energy = current_energy
+        else:
+            # Revert the swap if not accepted
+            current_state[swap_pos1], current_state[swap_pos2] = current_state[swap_pos2], current_state[swap_pos1]
+            accepted_moves.append(0)
+            stagnant_iterations += 1
+            
+        # Record the current objective value and temperature
+        objective_values.append(current_energy)
+        temperatures.append(T)
+        
+        # Update temperature and iteration count
+        T = T * cooling_rate
+        t += 1
+        
+        # Reheat if stuck in local optima
+        if stagnant_iterations >= reheat_threshold:
+            T = T_init * 0.75 # Increase the temperature
+            stagnant_iterations = 0  # Reset stagnation counter
     
     return best_state, objective_values, temperatures, accepted_moves
--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,44 +1,8 @@
 # Importing the required libraries
 import numpy as np
-import matplotlib.pyplot as plt
-from sudoku_generator import generate_random_sudoku, find_square
-from simulated_annealing import simulated_annealing as sa
-from functools import partial
 
-# Objective function
-def objective_function(sudoku):
-    row_scores = []
-    column_scores = []
-    n = sudoku.shape[0]
-    
-    for i in range(n):
-        row_scores.append(n - len(np.unique(sudoku[i, :])))
-        column_scores.append(n - len(np.unique(sudoku[:, i])))
-    return sum(row_scores) + sum(column_scores)
-
-#def delta_eval(sudoku, swap_pos1, swap_pos2):
-#    i, j = swap_pos1
-#    k, l = swap_pos2
-#    n = sudoku.shape[0]
-#    
-#    # Calculate the row and column scores before the swap
-#    row_score_after = n - len(np.unique(sudoku[i, :])) + n - len(np.unique(sudoku[k, :]))
-#    column_score_after = n - len(np.unique(sudoku[:, j])) + n - len(np.unique(sudoku[:, l]))
-#    
-#    # Swap the values
-#    sudoku[i, j], sudoku[k, l] = sudoku[k, l], sudoku[i, j]
-#    
-#    # Calculate the row and column scores after the swap
-#    row_score_before = n - len(np.unique(sudoku[i, :])) + n - len(np.unique(sudoku[k, :]))
-#    column_score_before = n - len(np.unique(sudoku[:, j])) + n - len(np.unique(sudoku[:, l]))
-#    
-#    # Calculate the change in the objective function
-#    delta_E = (row_score_after + column_score_after) - (row_score_before + column_score_before)
-#    
-#    # Revert the swap
-#    sudoku[i, j], sudoku[k, l] = sudoku[k, l], sudoku[i, j]
-#    return delta_E
-
+def find_square(i, j):
+    return i // 3, j // 3
 
 # Function to compute an initial candidate solution
 def initial_candidate_sudoku(sudoku):
@@ -55,8 +19,37 @@ def initial_candidate_sudoku(sudoku):
     return sudoku_copy
 
 
+def objective_function(sudoku):
+    """ The objective function is the sum of the number of repeated values in each row and column.
+        The objective function is minimized when the sudoku is solved.
+    
+    Args:
+        sudoku (np.array): The candidate solution to evaluate
+    
+    Returns:
+        int: The objective function value
+    """
+    row_scores = []
+    column_scores = []
+    n = sudoku.shape[0]
+    
+    for i in range(n):
+        row_scores.append(n - len(np.unique(sudoku[i, :])))
+        column_scores.append(n - len(np.unique(sudoku[:, i])))
+    return sum(row_scores) + sum(column_scores)
+
+
 # Neighborhood operator to mutate the candidate solution by a small amount
 def neighborhood_operator(sudoku, fixed_mask):
+    """ The neighborhood operator swaps two random cells in the same 3x3 grid of the sudoku.
+
+    Args:
+        sudoku (np.array): The candidate solution to mutate
+        fixed_mask (np.array): A binary mask indicating which cells are fixed
+
+    Returns:
+        tuple: The indices of the two cells that were swapped
+    """
     n = sudoku.shape[0]
     
     # Randomly select two cells to swap
@@ -67,6 +60,7 @@ def neighborhood_operator(sudoku, fixed_mask):
         
     r,c = find_square(i,j)
     k, l = np.random.randint(r * 3, r * 3 + 3), np.random.randint(c * 3, c * 3 + 3)
+    
     # Ensure we don't select a fixed cell or the same cell
     while (k, l) == (i, j) or fixed_mask[k, l]:
         k, l = np.random.randint(r * 3, r * 3 + 3), np.random.randint(c * 3, c * 3 + 3)
@@ -74,3 +68,33 @@ def neighborhood_operator(sudoku, fixed_mask):
     # Swap the values    
     sudoku[i, j], sudoku[k, l] = sudoku[k, l], sudoku[i, j]
     return (i, j), (k, l)
+
+def delta_evaluation(sudoku, swap1, swap2):
+    """ Evaluate the change in the objective function value after a swap operation.
+
+    Args:
+        sudoku (np.array): The candidate solution to evaluate
+
+    Returns:
+        int: The change in the objective function value
+    """
+    i, j  = swap1
+    k, l = swap2
+    n = sudoku.shape[0]
+    
+    # Compute the row and column scores differences
+    row_score_after =   (n - len(np.unique(sudoku[i, :])))  + (n - len(np.unique(sudoku[k, :]))) 
+    column_score_after =  (n - len(np.unique(sudoku[:, j]))) + (n - len(np.unique(sudoku[:, l])))
+    
+    sudoku[i, j], sudoku[k, l] = sudoku[k, l], sudoku[i, j]
+    
+    row_score_before =  (n - len(np.unique(sudoku[i, :]))) + (n - len(np.unique(sudoku[k, :])))
+    column_score_before = (n - len(np.unique(sudoku[:, j]))) + (n - len(np.unique(sudoku[:, l])))
+    
+    row_score_diff = row_score_after - row_score_before
+    column_score_diff = column_score_after - column_score_before
+    
+    
+    sudoku[i, j], sudoku[k, l] = sudoku[k, l], sudoku[i, j]
+    
+    return row_score_diff + column_score_diff
